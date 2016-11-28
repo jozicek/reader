20b2650f5358edc295d93dd81726c52a2fe6ee19
Jozef Krcmarik <j.krcmarik@gmail.com>
2016-11-28 12:45:37 +0100

    first commit, basic mvn project

---
+++ src/main/java/jozinko/pdf/App.java
@@ -0,0 +1,13 @@
+package jozinko.pdf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
